63714920844cf13ca4cb5c8899e2a7fac7d5fe2d
zy <2517652806@qq.com>
2022-12-15 12:17:37 +0800

    创建model和controller

---
+++ app/Models/OriginalSong.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OriginalSong extends Model
+{
+    //
+    // 指定数据表
+    protected $table = "original_song";
+    // 指定开启时间戳
+    public $timestamps = true;
+    // 指定主键
+    protected $primaryKey = "id";
+    // 指定不允许自动填充的字段，字段修改的黑名单
+    protected $guarded = [];
+}
