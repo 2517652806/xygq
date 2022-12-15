5784e2fdcb24f7d8cf1c565141559b8151e15257
Zt <496440059@qq.com>
2022-12-15 20:09:59 +0800

    zt first ok

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('registered','SchoolController@registered');//注册
+Route::post('school_login','SchoolController@login');//学校端登录
+Route::post('admin_login','AdminController@login');//管理员登录
+Route::post('login_province','ProvinceController@login');//管理员登录
+Route::post('login_city','CityController@login');//管理员登录
+Route::post('school_password','SchoolController@updatePassword')->middleware('jwt.role:user','jwt.auth');//学校端修改密码
+Route::post('admin_modify_account','SchoolController@updateSchool');//学校端修改学校账号
